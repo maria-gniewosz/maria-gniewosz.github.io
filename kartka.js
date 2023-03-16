9f4b0fe9d44e69f9d490855fe91c4287bce4ad8a
maria-gniewosz <84970200+maria-gniewosz@users.noreply.github.com>
2023-03-16 15:03:15 +0100

    Add files via upload

---
+++ kartka.js
@@ -0,0 +1,27 @@
+
+// import kaboom lib
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+kaboom();
+
+loadSprite("swienta","swienta.png")
+loadSprite("wombat2","wombat2.png")
+loadSound("muzyka", "muza.mp3")
+
+add([
+    sprite("swienta")
+])
+
+const wombat2 = add([
+    sprite("wombat2"),
+    pos(100,250)
+])
+
+add([play("muzyka")])
+
+onKeyRelease("space",()=>play ("muzyka"))
+
+onUpdate(()=>{
+    if(wombat2.pos.x<400)
+    wombat2.pos.x +=1
+})
