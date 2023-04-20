bf9ed722c6d3d8d710fcd50a10304b4277ef81ff
maria-gniewosz <84970200+maria-gniewosz@users.noreply.github.com>
2023-04-20 15:19:49 +0200

    Add files via upload

--- kartka.js
+++ kartka.js
@@ -25,3 +25,15 @@ onUpdate(()=>{
     if(wombat2.pos.x<400)
     wombat2.pos.x +=1
 })
+
+let wprawo = true
+
+player.onUpdate(()  =>{
+    if(wprawo && player.pos.x < 600)
+    player.pos.x += 1
+
+    else if(!wprawo && player.pos.x > 50)
+    player.pos.x -=1
+
+    else wprawo ^= true
+})
